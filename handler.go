de4ae0ee313990d5ca89469233ba368ee0c7d87d
Maciej 'Witcher' Plachta <maciej.plachta@aristocrat.com>
2018-03-02 11:35:50 +1100

    Solution for gophercises.com urlshoter exercise

--- handler.go
+++ handler.go
@@ -2,6 +2,8 @@ package urlshort
 
 import (
 	"net/http"
+
+	"gopkg.in/yaml.v2"
 )
 
 // MapHandler will return an http.HandlerFunc (which also
@@ -11,8 +13,14 @@ import (
 // If the path is not provided in the map, then the fallback
 // http.Handler will be called instead.
 func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
-	//	TODO: Implement this...
-	return nil
+	return func(w http.ResponseWriter, r *http.Request) {
+		dest, ok := pathsToUrls[r.URL.Path]
+		if !ok {
+			fallback.ServeHTTP(w, r)
+			return
+		}
+		http.Redirect(w, r, dest, http.StatusFound)
+	}
 }
 
 // YAMLHandler will parse the provided YAML and then return
@@ -32,6 +40,22 @@ func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.Handl
 // See MapHandler to create a similar http.HandlerFunc via
 // a mapping of paths to urls.
 func YAMLHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
-	// TODO: Implement this...
-	return nil, nil
+	var records []pathToURL
+	if err := yaml.Unmarshal(yml, &records); err != nil {
+		return nil, err
+	}
+	return func(w http.ResponseWriter, r *http.Request) {
+		for _, record := range records {
+			if r.URL.Path == record.Path {
+				http.Redirect(w, r, record.URL, http.StatusFound)
+				return
+			}
+		}
+		fallback.ServeHTTP(w, r)
+	}, nil
+}
+
+type pathToURL struct {
+	Path string `yaml:"path,omitempty"`
+	URL  string `yaml:"url,omitempty"`
 }
